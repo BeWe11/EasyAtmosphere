592d7b55653d50068cca0a0155f682a8f4a201f3
Benjamin Weigang <Benjamin.Weigang@gmx.de>
2016-02-04 16:27:53 +0100

    Initial Commit

---
+++ PathfinderSounds/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PathfinderSounds
+//
+//  Created by Benjamin Weigang on 04/02/16.
+//  Copyright © 2016 Benjamin Weigang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
